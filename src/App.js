008ccf53a2be7fcac94d0048a3cc53daf643396e
sweinstein27 <sweinstein27@gmail.com>
2019-04-04 09:36:38 -0700

    stocks state doesn't set

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stocks: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&outputsize=compact&apikey=3GD1L60R2ALY2NRG&datatype=json')
+    .then(response => response.json())
+    .then(data => this.setState({ stocks: data["Time Series (Daily)"] }));
+    debugger
+  }
+
   render() {
+    const { stocks } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      "hi"
     );
   }
+
+  
+
 }
+
 
 export default App;
